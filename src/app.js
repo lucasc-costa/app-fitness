cf66e5c170c4f624de2bfce8d5fa1fee52705567
lucasc-costa <62854155+lucasc-costa@users.noreply.github.com>
2026-02-26 22:02:40 -0300

    chore: setup Docker, docker-compose and PostgreSQL environment

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ app.get("/", function (req, res) {
   res.send("Olá mundo!");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Servidor iniciado!");
 });
